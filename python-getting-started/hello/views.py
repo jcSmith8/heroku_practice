5b23ae3b6d83e95d164b00f064d436c2d5399af1
Cooper Smith <60166434+jcSmith8@users.noreply.github.com>
2024-03-24 14:58:15 -0700

    edits
    tutorial steps

--- python-getting-started/hello/views.py
+++ python-getting-started/hello/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 import requests
 from django.http import HttpResponse
 from .models import Greeting
+import os
 
 # Create your views here.
 
 
 def index(request):
-    r = requests.get('https://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    times = int(os.environ.get('TIMES'))
+    return HttpResponse('Hello! ' * times)
 
 
 def db(request):
